a57d136ff26c22a17d49a0f125b55d5654580d50
Shaddi H <shaddih1@gmail.com>
2019-12-12 04:39:08 -0600

    🎉 v0.2 Coming soon

--- extracting.py
+++ extracting.py
@@ -16,7 +16,7 @@ def usage():
 	parser.add_argument("-f", "--file_name", metavar="FILE_NAME", help="Set file name")
 	if len(sys.argv) < 2:
 		print("Comming Soon v0.2")
-		sys.exit()
+		pass
 	return parser.parse_args()
 
 def main():
